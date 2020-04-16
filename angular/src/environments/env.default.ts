72c15e1d7b17513079b65613cfce6c465430fbbd
Minh Long <hominhlong.it@gmail.com>
2020-04-16 22:01:31 +0700

    Build new endpoint for front-end

--- angular/src/environments/env.default.ts
+++ angular/src/environments/env.default.ts
@@ -1,14 +1,11 @@
 export const environment = {
   production: false,
   versionCheckURL: '/version.json',
-  version: '2.2.7',
+  version: '2.2.8',
 
   // Auto check and refresh browser after 60 seconds
   versionFrequency: 1000 * 60,
 
-  // webURL: 'http://laravel.test',
-  // apiURL: 'http://laravel.test/v1',
-
-  webURL: 'https://api.tnttnamhoa.org',
-  apiURL: 'https://api.tnttnamhoa.org/v1',
+  webURL: 'http://laravel.test',
+  apiURL: 'http://laravel.test/v1',
 };
--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { environment as env } from './env.default';
 
 env.production = true;
-env.webURL = 'http://new-api.tnttnamhoa.org';
-env.apiURL = 'http://new-api.tnttnamhoa.org/v1';
+env.webURL = 'https://api.tnttnamhoa.org';
+env.apiURL = 'https://api.tnttnamhoa.org/v1';
 
 export const environment = env;
