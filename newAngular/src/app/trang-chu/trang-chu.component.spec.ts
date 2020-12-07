ea110ca36bc6e5ebed70f8eccbb97eab1f425f8b
Minh Long <hominhlong.it@gmail.com>
2020-12-07 11:45:03 +0700

    Upgrade to 11

--- newAngular/src/app/trang-chu/trang-chu.component.spec.ts
+++ newAngular/src/app/trang-chu/trang-chu.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TrangChuComponent } from './trang-chu.component';
 
@@ -6,7 +6,7 @@ describe('TrangChuComponent', () => {
   let component: TrangChuComponent;
   let fixture: ComponentFixture<TrangChuComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TrangChuComponent ]
     })
